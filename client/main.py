13980f7656e51b4c98c291394aa6a360c3dbcb17
Jasper Wallace <jasper@pointless.net>
2014-05-31 19:06:28 +0100

    daemonise + init script

--- client/main.py
+++ client/main.py
@@ -2,16 +2,15 @@
 #
 #
 
-import os, sys, time, logging, select, socket, textwrap
+import os, sys, time, logging, select, socket, textwrap, argparse
 from logging.handlers import SysLogHandler
+from daemon import DaemonContext
+from pidfile import PidFile
 from display import display
 from histlist import historylist
 from ifstatslinux import get_sys_stats
 from nickcolors import sixteenbit2fourbitcolour, nick2colour
 from iftable import get_speeds_snmp
-
-host = "boole"
-community = "public"
 
 class Speed:
     def __init__(self, callback, args, inspeed, outspeed, period = 5):
@@ -124,55 +123,14 @@ class Speed:
             ret.append(self.display_mangle(rate, link_speed))
         return ret
 
-logger = logging.root
-logger.setLevel(logging.DEBUG)
-syslog = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_DAEMON)
-formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
-syslog.setFormatter(formatter)
-logger.addHandler(syslog)
-
-logger.info("net-o-meter starting up.")
-
-if len(sys.argv) > 1:
-    iface = sys.argv[1]
-
-d = display()
-d.clear()
-d.start()
-d.bottom(0)
-d.top(0)
-d.centered("inbound", 0)
-d.centered("outbound", 2)
-
-ttuple = time.localtime()
-date = time.strftime("%a %e %b", ttuple)
-d.centered(date, 1)
-
-#
-# XXX get these automatticly
-#
-downspeed = 80.0
-upspeed = 20.0
-
-period = 5
-tperiod = period # target period
-
-host = "boole"
-
-#speeds = (('ppp0', 80.0, 20.0), ('ppp1', 80.0, 20.0))
-zeniface = Speed(get_speeds_snmp, [host, 'ppp0'], downspeed, upspeed)
-
-# I'm not sure about the up and down speeds
-# but i think this is right.
-aaiface = Speed(get_speeds_snmp, [host, 'ppp1'], 40.0, 10.0)
-
-ifaces = [["Zen (IPv4)", zeniface, 0], ["A&A (IPv6)", aaiface, 0]]
-
-for ispname, iface, c in ifaces:
-    iface.update()
-
-ntime = time.time()
-time.sleep(period)
+def set_logger():
+    logger = logging.root
+    logger.setLevel(logging.DEBUG)
+    syslog = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_DAEMON)
+    formatter = logging.Formatter('netometer[%(process)d]: %(levelname)-8s %(message)s')
+    syslog.setFormatter(formatter)
+    logger.addHandler(syslog)
+    return logger
 
 def bcast_socket(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -182,127 +140,196 @@ def bcast_socket(port):
     s.setblocking(0)
     return s
 
-s = bcast_socket(50000)
-s_bell = bcast_socket(50002)
+def daemonise():
+    from daemon import DaemonContext
+    from pidfile import PidFile
+    daemon = DaemonContext(pidfile=PidFile("/var/run/netometer.pid"))
+    daemon.open()
+    logging.info('Daemonised netometer')
 
-state = "bw"
-statecount = 0
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Display bandwidth usage on the bandwidth meter.')
+    parser.add_argument('--host', type=str, nargs=1, metavar=('<hostname>'),
+      help='the host to read bandwidth from')
 
-event = serial = name = None
-iselect = 0
-changed = True
-nchunks = None
-colour = None
+    parser.add_argument('--community', type=str, nargs=1, metavar=('<community>'),
+      help='community name to use')
+    
+    args = parser.parse_args()
 
-wrapper = textwrap.TextWrapper(width=20, expand_tabs=False)
+    logger = set_logger()
 
-while True:
+    logging.info("net-o-meter starting up.")    
+
+    try:
+        daemonise()
+    except Exception, e:
+        logging.exception("Exception in main loop: %s" % e)
+    
+    logging.info("daemonised.")
+
+    d = display()
+    d.clear()
+    d.start()
+    d.bottom(0)
+    d.top(0)
+    d.centered("inbound", 0)
+    d.centered("outbound", 2)
+
+    ttuple = time.localtime()
+    date = time.strftime("%a %e %b", ttuple)
+    d.centered(date, 1)
+
+    if 'hostname' in args:
+        host = args.hostname
+    else:
+        host = 'boole'
+
+    #
+    # XXX get these automatticly
+    #
+    downspeed = 80.0
+    upspeed = 20.0
+
+    period = 5
+    tperiod = period # target period
+    
+    #speeds = (('ppp0', 80.0, 20.0), ('ppp1', 80.0, 20.0))
+    zeniface = Speed(get_speeds_snmp, [host, 'ppp0'], downspeed, upspeed)
+
+    # I'm not sure about the up and down speeds
+    # but i think this is right.
+    aaiface = Speed(get_speeds_snmp, [host, 'ppp1'], 40.0, 10.0)
+
+    ifaces = [["Zen (IPv4)", zeniface, 0], ["A&A (IPv6)", aaiface, 0]]
+
     for ispname, iface, c in ifaces:
         iface.update()
-    otime = ntime
+
     ntime = time.time()
+    time.sleep(period)
 
-    ispname, iface, counter = ifaces[iselect]
-    ifaces[iselect][2] += 1
-    if iselect == 1:
-        iselect = 0
-    else:
-        iselect = 1
+    s = bcast_socket(50000)
+    s_bell = bcast_socket(50002)
 
-    ispeed, ospeed = iface.rate_mbit()
-    indisplay, outdisplay = iface.rate_display()
+    state = "bw"
+    statecount = 0
 
-    legend = "%d Secs sample" % (tperiod)
+    event = serial = name = None
+    iselect = 0
+    changed = True
+    nchunks = None
+    colour = None
 
-    # every other time
-    if counter % 2 and iface.ominlist.full():
-        (ispeed, ospeed) = iface.onemin_mbit()
-        indisplay, outdisplay = iface.onemin_display()
-        legend = "1 Min average"
+    wrapper = textwrap.TextWrapper(width=20, expand_tabs=False)
 
-    if ((counter % 6) == 0) and iface.otenminlist.full():
-        (ispeed, ospeed) = iface.tenmin_mbit()
-        indisplay, outdisplay = iface.tenmin_display()
-        legend = "10 Min average"
+    while True:
+        for ispname, iface, c in ifaces:
+            iface.update()
+        otime = ntime
+        ntime = time.time()
 
-    # these are in and out on the port
-    t = "Down " + str(ispeed) + " Mbits"
-    b = "Up   " + str(ospeed) + " Mbits"
+        ispname, iface, counter = ifaces[iselect]
+        ifaces[iselect][2] += 1
+        if iselect == 1:
+            iselect = 0
+        else:
+            iselect = 1
 
-    d.clear()
+        ispeed, ospeed = iface.rate_mbit()
+        indisplay, outdisplay = iface.rate_display()
 
-    if state == "card":
-        if changed:
-            name = name.replace('\n', ' ')
-            colour = sixteenbit2fourbitcolour(nick2colour(name))
-            name = name + " Opened the %s door." % (source)
-            # the display is 20 chars accross
-            nchunks = wrapper.wrap(name)
-            if len(nchunks) > 3:
-                nchunks = nchunks[0:3]
-        y = 0
-        for n in nchunks:
-            d.left(n, y)
-            y += 1
-        d.strip('t', (colour,) * 16)
-        d.strip('b', (colour,) * 16)
-    elif state == "bell":
-        d.left("Somebody is", 0)
-        d.left("at the door.", 1)
-        d.strip('t', ('000', '0f0') * 8)
-        d.strip('b', ('0f0', '000') * 8)        
-    else:
-        d.left(t, 0)
-        d.left("ISP: " + ispname, 1)
-        d.left(b, 2)
-        d.left(legend, 3)
-        d.top(indisplay)
-        d.bottom(outdisplay)
+        legend = "%d Secs sample" % (tperiod)
 
-    if statecount > 0:
-        statecount -= 1
-        if statecount == 0:
-            state = "bw"
+        # every other time
+        if counter % 2 and iface.ominlist.full():
+            (ispeed, ospeed) = iface.onemin_mbit()
+            indisplay, outdisplay = iface.onemin_display()
+            legend = "1 Min average"
 
-    real_period = ntime - otime
+        if ((counter % 6) == 0) and iface.otenminlist.full():
+            (ispeed, ospeed) = iface.tenmin_mbit()
+            indisplay, outdisplay = iface.tenmin_display()
+            legend = "10 Min average"
 
-    pdiff = real_period - period
-    period = tperiod - pdiff
+        # these are in and out on the port
+        t = "Down " + str(ispeed) + " Mbits"
+        b = "Up   " + str(ospeed) + " Mbits"
 
-    if period < 2:
-        continue
+        d.clear()
 
-    result = select.select([s, s_bell], [], [], period)
-    source = None
-    changed = False
-    if len(result[0]) != 0:
-        changed = True
-        payload = result[0][0].recv(1024)
+        if state == "card":
+            if changed:
+                name = name.replace('\n', ' ')
+                colour = sixteenbit2fourbitcolour(nick2colour(name))
+                name = name + " Opened the %s door." % (source)
+                # the display is 20 chars accross
+                nchunks = wrapper.wrap(name)
+                if len(nchunks) > 3:
+                    nchunks = nchunks[0:3]
+            y = 0
+            for n in nchunks:
+                d.left(n, y)
+                y += 1
+            d.strip('t', (colour,) * 16)
+            d.strip('b', (colour,) * 16)
+        elif state == "bell":
+            d.left("Somebody is", 0)
+            d.left("at the door.", 1)
+            d.strip('t', ('000', '0f0') * 8)
+            d.strip('b', ('0f0', '000') * 8)        
+        else:
+            d.left(t, 0)
+            d.left("ISP: " + ispname, 1)
+            d.left(b, 2)
+            d.left(legend, 3)
+            d.top(indisplay)
+            d.bottom(outdisplay)
 
-        # we only read from the 1st id.
-        # if we get 2 packets at the same time do we lose the 2nd?
-        if result[0][0] == s:
-            source = "back"
+        if statecount > 0:
+            statecount -= 1
+            if statecount == 0:
+                state = "bw"
 
-        if result[0][0] == s_bell:
-            source = "front"
+        real_period = ntime - otime
 
-        try:
-            (event, serial, name) = payload.split("\n")
-        except ValueError:
-            logger.error("Unparsable payload: >%s<" % payload )
-            event = None
-            serial = None
-            name = None
-        if payload.find("56C00DBA") > -1:
-            # hello paddy
-            event = 'RFID'
-            serial = '56C00DBA'
-            name = 'Padski'
-        logger.info("%s: %s %s %s" % (source, event, serial, name))
-        if (event == 'RFID' and name):
-            state = "card"
-            statecount = 2
-        elif (event == 'BELL'):
-            state = "bell"
-            statecount = 2
+        pdiff = real_period - period
+        period = tperiod - pdiff
+
+        if period < 2:
+            continue
+
+        result = select.select([s, s_bell], [], [], period)
+        source = None
+        changed = False
+        if len(result[0]) != 0:
+            changed = True
+            payload = result[0][0].recv(1024)
+
+            # we only read from the 1st id.
+            # if we get 2 packets at the same time do we lose the 2nd?
+            if result[0][0] == s:
+                source = "back"
+
+            if result[0][0] == s_bell:
+                source = "front"
+
+            try:
+                (event, serial, name) = payload.split("\n")
+            except ValueError:
+                logger.error("Unparsable payload: >%s<" % payload )
+                event = None
+                serial = None
+                name = None
+            if payload.find("56C00DBA") > -1:
+                # hello paddy
+                event = 'RFID'
+                serial = '56C00DBA'
+                name = 'Padski'
+            logger.info("%s: %s %s %s" % (source, event, serial, name))
+            if (event == 'RFID' and name):
+                state = "card"
+                statecount = 2
+            elif (event == 'BELL'):
+                state = "bell"
+                statecount = 2
